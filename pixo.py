72b23ffa131dada2dd7c3abea15e585f34d56dfa
Hojiakbar <hojiakbarxasanboyev785@gmail.com>
2026-03-09 11:45:45 +0500

    Update pixo.py

--- pixo.py
+++ pixo.py
@@ -3,11 +3,12 @@ import yt_dlp
 import os
 
 TOKEN = "8624963114:AAGR01L6bgJdKGPYTnSHuH7Z65JgxzDCwj0"  # BotFather dan olingan token
-
 bot = telebot.TeleBot(TOKEN)
 
 DOWNLOAD_FOLDER = "downloads"
 os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
+
+users = set()  # foydalanuvchilarni saqlash
 
 ydl_opts = {
     "format": "best",
@@ -22,7 +23,6 @@ def download_video(url):
         info = ydl.extract_info(url, download=True)
         filename = ydl.prepare_filename(info)
         title = info.get("title", "Video")
-
     return filename, title
 
 
@@ -36,26 +36,24 @@ def safe_remove(path):
 
 @bot.message_handler(commands=["start"])
 def start(message):
+    users.add(message.from_user.id)  # foydalanuvchini qo‘shish
     bot.send_message(
         message.chat.id,
-        "🎬 Video Downloader Bot\n\n"
-        "📥 Video link yuboring (YouTube / TikTok / Instagram)"
+        f"Salom, men Pixo video yuklovchi botman!\n"
+        f"Foydalanuvchilar soni: {len(users)}"
     )
 
 
 @bot.message_handler(func=lambda m: True)
 def handler(message):
-
     url = message.text.strip()
+    users.add(message.from_user.id)
 
     msg = bot.reply_to(message, "⏳ Video yuklanmoqda...")
-
     file_path = None
 
     try:
-
         file_path, title = download_video(url)
-
         with open(file_path, "rb") as video:
             bot.send_video(
                 message.chat.id,
@@ -63,5 +61,18 @@ def handler(message):
                 caption=title,
                 supports_streaming=True
             )
+        bot.delete_message(message.chat.id, msg.message_id)
 
-        bot
+    except Exception as e:
+        bot.edit_message_text(
+            f"❌ Xato:\n{e}",
+            message.chat.id,
+            msg.message_id
+        )
+
+    finally:
+        safe_remove(file_path)
+
+
+print("🚀 Pixo video bot ishga tushdi")
+bot.infinity_polling(skip_pending=True)
